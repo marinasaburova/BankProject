635612b8e752e9353eb93fdc0eb050d6a9be25b2
Marina Saburova <79031317+marinasaburova@users.noreply.github.com>
2022-04-14 15:06:12 -0400

    minor change

--- Admin/Pages/dashboard.php
+++ Admin/Pages/dashboard.php
@@ -184,7 +184,7 @@ include '../view/navigation.php';
                     <!-- /.card-body -->
                     <div class="card-footer clearfix">
                         <a href="bank-transaction.php" class="btn btn-sm btn-info float-left">Make a Transaction</a>
-                        <a href="transaction-history.php" class="btn btn-sm btn-secondary float-right">View All History</a>
+                        <a href="transactions.php" class="btn btn-sm btn-secondary float-right">View All History</a>
                     </div>
                     <!-- /.card-footer -->
                 </div>
