f4eb301536f2751cf6dd0a17781681ec9b30040a
mennaelzomor <98981548+mennaelzomor@users.noreply.github.com>
2022-05-08 11:12:08 -0400

    Update new-bankacct.php

--- customer/Pages/new-bankacct.php
+++ customer/Pages/new-bankacct.php
@@ -16,10 +16,10 @@ $title = "Open Bank Account";
 include '../view/header-simple.php';
 
 if (isset($_GET['msg'])) {
-    if (trim($_GET['msg']) == 'error') {
+    if addslashes(trim($_GET['msg']) == 'error') {
         $error = 'Your account could not be created. Please try again.';
     }
-    if (trim($_GET['msg']) == 'success') {
+    if addslashes(trim($_GET['msg']) == 'success') {
         $success = 'Your account request has been submitted. Please wait for an employee to approve it.';
     }
 }
